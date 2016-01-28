962c0df23512b955f9affb9c2078f5837374444e
riti verma <riti6266@gmail.com>
2016-01-29 00:09:48 +0530

    updated files

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
 $(document).ready(function(){
 	$('.bxSlider').bxSlider({
-		mode:'horizontal',
+		mode:'vertical',
 		useCSS:false,
 		infiniteLoop:true,
-		hideControlOnEnd:true,
-		speed:500
+		hideControlOnEnd:false,
+		auto:true,
+		autoControls:true,
+		mode:'fade',
+		speed:5000,
+		pause:false,
+		slideWidth:320,
+		slideMargin:10,
+			});
+			
+			 $('.outside').on({
+     mouseover:function()
+      {
+            $('.outside').css('Cursor','hand');
+              $('.outside').css('border-color','white');
+             
+               
+      },
+      mouseout:function()
+      {
+              $('.outside').css({
+                'cursor':'pointer',
+                'border-color':'gray'
+              });
+              
+      }
+    });
+			 $(".content").show(function(){
+			 	$("p").fadeIn(1000);
+			 	/*,function(){
+			 		$("p").fadeIn(4000);*/
+			 	});
+			 	return false;
+			 
+	 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+		$('.ans-display').append($('.G-ques').append("<p>"+question+ "</p>"+"<br>"+" <br>" + "<li>"+option+ "<p>"+answer+"</p>"+"</li>"));
 	});
-});
+  
+
+	$('.countries').on('click',function(){
+		$('.C-ques').insertAfter(".ans-display").delay(5000).hide();
+
+	});
+
+	*/
+   
